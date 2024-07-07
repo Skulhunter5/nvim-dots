981feb6b5cf3e8b689ac7ded29631b52406fba67
Skulhunter5 <linus.huster@gmail.com>
2024-07-07 06:40:59 +0200

    Fix nixos

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,38 +1,40 @@
 local utils = require("utils")
 
-return {
-    (not utils.is_nixos()) and {
-        {
-            "williamboman/mason.nvim",
-            config = function()
-                require("mason").setup()
-            end,
-        },
-        {
-            "williamboman/mason-lspconfig.nvim",
-            config = function()
-                require("mason-lspconfig").setup({
-                    ensure_installed = { "lua_ls", "rust_analyzer" },
-                })
-            end,
-        },
-    },
+local lspconfig = {
+    "neovim/nvim-lspconfig",
+    config = function()
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.rust_analyzer.setup({
+            capabilities = capabilities,
+        })
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+}
+
+return (not utils.is_nixos()) and {
     {
-        "neovim/nvim-lspconfig",
+        "williamboman/mason.nvim",
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities,
-            })
-
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            require("mason").setup()
         end,
     },
-}
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "rust_analyzer" },
+            })
+        end,
+    },
+    lspconfig,
+} or lspconfig
+
+
