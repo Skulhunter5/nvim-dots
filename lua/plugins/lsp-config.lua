2288d5e0ba932325ea7f99e1f77c99ba06aca53d
Skulhunter5 <linus.huster@gmail.com>
2024-08-14 13:48:34 +0200

    Enable and configure inlay hints

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,54 @@ local lspconfig = {
         })
         lspconfig.rust_analyzer.setup({
             capabilities = capabilities,
+            on_attach = function(_, bufnr)
+                vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+            end,
+            settings = {
+                ["rust-analyzer"] = {
+                    cargo = {
+                        allFeatures = true,
+                    },
+                    -- mostly default but some changes, so just to be safe
+                    inlayHints = {
+                        bindingModeHints = { enable = false },
+                        chainingHints = { enable = true },
+                        closingBraceHints = {
+                            enable = true,
+                            minLines = 25
+                        },
+                        closureCaptureHints = { enable = false },
+                        closureReturnTypeHints = { enable = "never" },
+                        closureStyle = "impl_fn",
+                        discriminantHints = { enable = "never" },
+                        expressionAdjustmentHints = {
+                            enable = "never",
+                            hideOutsideUnsafe = false,
+                            mode = "prefix"
+                        },
+                        genericParameterHints = {
+                            const = { enable = false },
+                            lifetime = { enable = false },
+                            type = { enable = false },
+                        },
+                        implicitDrops = { enable = false },
+                        lifetimeElisionHints = {
+                            enable = "never",
+                            useParameterNames = false
+                        },
+                        maxLength = 25,
+                        parameterHints = { enable = false },
+                        rangeExclusiveHints = { enable = false },
+                        reborrowHints = { enable = false },
+                        renderColons = true,
+                        typeHints = {
+                            enable = false,
+                            hideClosureInitialization = false,
+                            hideNamedConstructor = false,
+                        },
+                    },
+                },
+            },
         })
         lspconfig.zls.setup({
             capabilities = capabilities,
