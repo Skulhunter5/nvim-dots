3823cb41dcb310cad6b0dc87863052fb26a56a56
Skulhunter5 <linus.huster@gmail.com>
2025-11-24 18:59:43 +0100

    Add keybind for telescope lsp diagnostics

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
 
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fe", builtin.diagnostics, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
